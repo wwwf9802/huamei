ec43311320b0e1bf15bfc4d287ab8f7b5281ac10
anye18 <516282787@qq.com>
2017-04-06 18:28:29 +0800

    gtt

---
+++ js/global.js
@@ -0,0 +1,33 @@
+
+function getBaseServerUrl()
+{
+	return "http://192.168.1.55/eidpws";
+}
+function getImgBaseServerUrl()
+{
+	return "http://192.168.1.55";
+}
+
+
+
+function isEmpty(str)
+{
+	if(str == "" || str == null || str == undefined ||  str == "undefined"){
+		return true;
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
