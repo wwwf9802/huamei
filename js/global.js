7b4ba904ec566c6cc5cce1ede9812c31b220a95f
yhn <2461379674@qq.com>
2017-04-11 15:03:44 +0800

    456

--- js/global.js
+++ js/global.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+function getBaseServerUrl() {
+	return "http://192.168.1.117:8090/eidpws";
+=======
 
 function getBaseServerUrl1()
 {
@@ -15,28 +19,27 @@ function getBaseServerUrl()
 function getImgBaseServerUrl()
 {
 	return "http://192.168.1.146:8081";
+>>>>>>> 5390d147bf1b18f004f3943af98a528922ae5892
 }
 
+function getImgBaseServerUrl() {
+	return "http://192.168.1.117:8090";
+}
 
-
-function isEmpty(str)
-{
-	if(str == "" || str == null || str == undefined ||  str == "undefined"){
+function isEmpty(str) {
+	if(str == "" || str == null || str == undefined || str == "undefined") {
 		return true;
 	}
 	return false;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//计算天数差的函数，通用  
+function DateDiff(sDate1, sDate2) { //sDate1和sDate2是2006-12-18格式  
+	var aDate, oDate1, oDate2, iDays
+	aDate = sDate1.split("-")
+	oDate1 = new Date(aDate[0] + '-' + aDate[1] + '-' + aDate[2]) //转换为12-18-2006格式  
+	aDate = sDate2.split("-")
+	oDate2 = new Date(aDate[0] + '-' + aDate[1] + '-' + aDate[2])
+	iDays = parseInt((oDate1 - oDate2) / 1000 / 60 / 60 / 24) //把相差的毫秒数转换为天数  
+	return iDays;
+}
